3fa57aedb79ce6a1ce5a450302cf0588116b6a88
Palili <parisasafipour1999@gmail.com>
2024-02-03 00:13:54 +0330

    feature: add contact page

---
+++ js/contact.js
@@ -0,0 +1,35 @@
+const form = document.getElementById("contact-us-form");
+const successMessage = document.getElementById("successMessage");
+const errorMessage = document.getElementById("errorMessage");
+
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+
+  const name = e.target.name.value;
+  const email = e.target.email.value;
+  const title = e.target.title.value;
+  const text = e.target.text.value;
+
+  fetch("http://localhost:3000/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, title, text }),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        errorMessage.style.display = 'none';
+        successMessage.style.display = "block";
+
+        form.reset();
+      } else {
+        successMessage.style.display = 'none';
+
+        errorMessage.innerText = res.error;
+        errorMessage.style.display = "block";
+      }
+    })
+};
